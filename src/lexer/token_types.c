e4032ba231095668df789d4a52ba5ff743c3de8f
Kateryna Zinchuk <kzinchuk@f1r1s7.codam.nl>
2025-04-30 18:25:09 +0200

    divide token logic to the smaller part due to the quoting implementation

--- src/lexer/token_types.c
+++ src/lexer/token_types.c
@@ -6,7 +6,7 @@
 /*   By: kzinchuk <kzinchuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 14:42:00 by kzinchuk          #+#    #+#             */
-/*   Updated: 2025/04/30 14:13:14 by kzinchuk         ###   ########.fr       */
+/*   Updated: 2025/04/30 18:24:28 by kzinchuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -66,30 +66,113 @@ void add_redirection_token(t_token_list *list, t_str_pos *lexer)
 	}
 }
 
+// void add_word_token(t_token_list *list, t_str_pos *lexer)
+// {
+// 	t_token	*new_token;
+// 	char *word;
+	
+// 	if (lexer->input[lexer->current] == '"' || lexer->input[lexer->current] == '\'')
+// 	{
+// 		return(check_quotes(lexer));
+// 	}	
+// 	lexer->start = lexer->current;
+// 	while (lexer->input[lexer->current] &&
+// 		!is_whitespace(lexer->input[lexer->current]) &&
+// 		lexer->input[lexer->current] != '|' &&
+// 		lexer->input[lexer->current] != '>' &&
+// 		lexer->input[lexer->current] != '<' &&
+// 		lexer->input[lexer->current] != '"')	
+// 	{
+// 		lexer->current++;
+// 	}
+// 	lexer->len = lexer->current - lexer->start;
+// 	if (lexer->len <= 0)
+// 		return;
+// 	word = ft_strndup(lexer->input + lexer->start, lexer->len);
+// 	if (!word)
+// 		return;
+// 	new_token = create_token(word, TOKEN_WORD, QUOTE_NONE);
+// 	free(word);
+// 	if (!new_token)
+// 		return;
+// 	add_to_token_list(list, new_token);
+// }
+
 void add_word_token(t_token_list *list, t_str_pos *lexer)
 {
-	t_token	*new_token;
-	char *word;
-	
+	if (use_quotes(lexer))
+	{
+		if (!check_quotes(lexer))
+			return(no_quotes_error(lexer));
+		return (add_quoted_word(list, lexer));
+	}
+	return (add_unquoted_word(list, lexer));
+}
+
+int use_quotes(t_str_pos *lexer)
+{
+	if (lexer->input[lexer->current] == '"' || lexer->input[lexer->current] == '\'')
+	{
+		return (1);
+	}
+	return (0);
+}
+
+int check_quotes(t_str_pos *lexer)
+{
+	int i;
+	char quote;
+
+	i = lexer->current;
+	quote = lexer->input[i];
+	i++;
+	while(lexer->input[i])
+	{
+		if (lexer->input[i] == quote)
+			return (1);
+		i++;
+	}
+	return (0);
+}
+int no_quotes_error(t_str_pos *lexer)
+{
+	printf("Error: No matching quotes found for token starting at index %d\n", lexer->start);
+	return (0);
+}
+t_token *add_quoted_word(t_token_list *list, t_str_pos *lexer)
+{
+	// add logic and test
+	return (word_token(list, lexer, type));
+}
+
+t_token *add_unquoted_word(t_token_list *list, t_str_pos *lexer)
+{
 	lexer->start = lexer->current;
-	while (lexer->input[lexer->current] &&
-		!is_whitespace(lexer->input[lexer->current]) &&
-		lexer->input[lexer->current] != '|' &&
-		lexer->input[lexer->current] != '>' &&
-		lexer->input[lexer->current] != '<' &&
-		lexer->input[lexer->current] != '"')	
+	while (lexer->input[lexer->current] && !is_whitespace(lexer->input[lexer->current]) &&
+		lexer->input[lexer->current] != '|' && lexer->input[lexer->current] != '>' &&
+		lexer->input[lexer->current] != '<' && lexer->input[lexer->current] != '"' &&
+		lexer->input[lexer->current] != '\'')
 	{
 		lexer->current++;
 	}
+	return(word_token(list, lexer, QUOTE_NONE));
+}
+t_token * word_token (t_token_list *list, t_str_pos *lexer, t_quote_type quote)
+{
+	t_token	*new_token;
+	char	*word;
+	int		start;
+
+	start = lexer->current;
 	lexer->len = lexer->current - lexer->start;
-	if (lexer->len <= 0)
-		return;
-	word = ft_strndup(lexer->input + lexer->start, lexer->len);
+	word = ft_strndup(lexer->input + start, lexer->len);
 	if (!word)
-		return;
+		return (NULL);
 	new_token = create_token(word, TOKEN_WORD, QUOTE_NONE);
 	free(word);
 	if (!new_token)
-		return;
+		return (NULL);
 	add_to_token_list(list, new_token);
+	return (new_token);
 }
+	
