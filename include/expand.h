026bbf4725fc470abaa1e4c6310827ece832fc51
Kateryna Zinchuk <zinchuck.k@gmail.com>
2025-06-26 14:05:40 +0200

    refactor expand_value function

--- include/expand.h
+++ include/expand.h
@@ -6,7 +6,7 @@
 /*   By: kzinchuk <kzinchuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/11 13:34:24 by tchernia          #+#    #+#             */
-/*   Updated: 2025/06/25 17:02:27 by kzinchuk         ###   ########.fr       */
+/*   Updated: 2025/06/26 13:59:54 by kzinchuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,11 @@ typedef struct s_expand_type
 /* -------------------------------------------------------------------------- */
 
 int		expand_segments(t_session *session);
+int		process_expansion_loop(char *raw, t_expand_type *exp, \
+									t_shell *init_shell);
+void	handle_double_dollar_sign(char *raw, t_expand_type *exp);
 char	*expand_value(char *raw, t_shell *shell);
+int		process_var(char *raw, t_expand_type *exp, t_shell *shell);
 void	extract_var(char *raw, t_expand_type *exp);
 void	expand_var(t_expand_type *exp, t_shell *shell);
 void	append_exp_str(t_expand_type *exp);
---
+++ src/expander/expand_segment.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   expand_segment.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: kzinchuk <kzinchuk@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/06/26 13:40:38 by kzinchuk          #+#    #+#             */
+/*   Updated: 2025/06/26 14:04:03 by kzinchuk         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+int	expand_segments(t_session *session)
+{
+	t_token		*cur;
+	t_segment	*seg;
+	char		*tmp;
+
+	cur = session->tokens->head;
+	while (cur)
+	{
+		if (cur->prev && cur->prev->type == T_HEREDOC)
+		{
+			cur = cur->next;
+			continue ;
+		}
+		seg = cur->segment;
+		while (seg)
+		{
+			if (ft_strchr(seg->value, '$') && seg->q_type != Q_SINGLE)
+			{
+				if (check_subs(seg->value))
+					return (check_error(BAD_SUBS, seg->value, GENERAL));
+				else
+					tmp = expand_value(seg->value, session->shell);//TODO what happens if there will be NULL?
+			}
+			else
+				tmp = ft_strdup(seg->value);
+			if (!tmp)
+			{
+				free(seg->value);
+				return (check_error(ENOMEM, "create tokens: ", GENERAL));
+			}
+			free(seg->value);
+			seg->value = tmp;
+			seg = seg->next;
+		}
+		cur = cur->next;
+	}
+	return (0);
+}
+
+int	process_expansion_loop(char *raw, t_expand_type *exp, t_shell *init_shell)
+{
+	while (raw[exp->i])
+	{
+		if (raw[exp->i] == '$')
+		{
+			if (raw[exp->i + 1] == '$' || is_whitespace(raw[exp->i + 1]) \
+				|| raw[exp->i + 1] == '\0')
+			{
+				handle_double_dollar_sign(raw, exp);
+			}
+			else
+			{
+				exp->i++;
+				if (process_var(raw, exp, init_shell))
+				{
+					error_free(exp);
+					return (1);
+				}
+				exp->i += exp->len_var;
+				free_exp(exp);
+			}
+		}
+		else
+			exp->res[exp->j++] = raw[exp->i++];
+	}
+	return (0);
+}
+
+void	handle_double_dollar_sign(char *raw, t_expand_type *exp)
+{
+	while (raw[exp->i + 1] && raw[exp->i + 1] == '$')
+		exp->res[exp->j++] = raw[exp->i++];
+	exp->res[exp->j++] = raw[exp->i++];
+}
+
+char	*expand_value(char *raw, t_shell *shell)
+{
+	t_expand_type	exp;
+
+	if (init_exp(&exp, raw))
+		return (NULL);
+	if (process_expansion_loop(raw, &exp, shell))
+	{
+		error_free(&exp);
+		return (NULL);
+	}
+	exp.res[exp.j] = '\0';
+	return (exp.res);
+}
