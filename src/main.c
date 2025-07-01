99c551f450b7756a1bfd4e89f700124b6f73f1d0
tetiana-cherni <tetiana.cherni@gmail.com>
2025-07-01 16:09:29 +0200

    reorder commits in main

--- src/main.c
+++ src/main.c
@@ -6,25 +6,12 @@
 /*   By: tchernia <tchernia@student.codam.nl>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/29 16:11:01 by kzinchuk          #+#    #+#             */
-/*   Updated: 2025/07/01 15:26:54 by tchernia         ###   ########.fr       */
+/*   Updated: 2025/07/01 15:58:20 by tchernia         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-/*
---leak-check=full
---track-origins=yes
-
-valgrind --leak-check=full --show-leak-kinds=all ./minishell
-
-valgrind --leak-check=full --show-leak-kinds=all --suppressions=readline.supp ./minishell
-
-valgrind --leak-check=full --show-leak-kinds=all --track-origins=yes --track-fds=yes -s --suppressions=readline.supp ./minishell
-
-
---track-fds=yes
-*/
 
 int	main(int argc, char **argv, char **env)
 {
