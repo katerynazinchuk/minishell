c9085d14293dd12ed7964dbe6e39843867623d2e
Kateryna Zinchuk <zinchuck.k@gmail.com>
2025-07-03 18:10:02 +0200

    refactor for norminette

--- src/utils/utils_main.c
+++ src/utils/utils_main.c
@@ -6,33 +6,33 @@
 /*   By: kzinchuk <kzinchuk@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/19 19:25:07 by tchernia          #+#    #+#             */
-/*   Updated: 2025/07/02 18:45:02 by kzinchuk         ###   ########.fr       */
+/*   Updated: 2025/07/03 18:09:25 by kzinchuk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-void ignore_args(int argc, char **argv)
+void	ignore_args(int argc, char **argv)
 {
 	(void)argc;
 	(void)argv;
 }
 
-int shell_exit(t_session *session)
+int	shell_exit(t_session *session)
 {
 	write(1, "exit\n", 5);
 	free_for_fork(session);
 	return (1);
 }
 
-void destroy_shell(t_shell *shell)
+void	destroy_shell(t_shell *shell)
 {
 	free_env_list(shell->env_list);
 	shell->env_list = NULL;
 	destroy_fd(shell->fd);
 }
 
-void destroy_fd(int *fd)
+void	destroy_fd(int *fd)
 {
 	close(fd[0]);
 	close(fd[1]);
